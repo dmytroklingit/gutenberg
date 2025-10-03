d1b4edba0cbd077575a539680857e55632352dc2
Daniel Richards <daniel.richards@automattic.com>
2025-10-03 14:36:46 +0800

    Show finish editing button for any child block of the temporarily edited block

--- packages/block-editor/src/components/block-inspector/index.js
+++ packages/block-editor/src/components/block-inspector/index.js
@@ -313,10 +313,8 @@ const BlockInspectorSingleBlock = ( {
 	hasBlockStyles,
 } ) => {
 	const hasMultipleTabs = availableTabs?.length > 1;
-
 	const blockInformation = useBlockDisplayInformation( clientId );
 	const isBlockSynced = blockInformation.isSynced;
-
 	const shouldShowTabs = ! isBlockSynced && hasMultipleTabs;
 
 	return (
