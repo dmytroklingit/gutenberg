64a523a8048b72d3447291e348117153536bf887
Daniel Richards <daniel.richards@automattic.com>
2025-10-07 15:01:07 +0800

    Try disabling blocks other than the temporarily edited blocks

--- packages/block-editor/src/components/block-list/index.js
+++ packages/block-editor/src/components/block-list/index.js
@@ -129,9 +129,13 @@ function StopEditingAsBlocksOnOutsideSelect( { clientId } ) {
 	const { stopEditingAsBlocks } = unlock( useDispatch( blockEditorStore ) );
 	const isBlockOrDescendantSelected = useSelect(
 		( select ) => {
-			const { isBlockSelected, hasSelectedInnerBlock } =
-				select( blockEditorStore );
+			const {
+				isBlockSelected,
+				hasSelectedInnerBlock,
+				getBlockSelectionStart,
+			} = select( blockEditorStore );
 			return (
+				! getBlockSelectionStart() ||
 				isBlockSelected( clientId ) ||
 				hasSelectedInnerBlock( clientId, true )
 			);
