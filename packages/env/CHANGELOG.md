99bec1b3e0f91c049ec58aa9b5669a4554fcfa78
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-10-22 19:00:28 +0900

    Env: Fix `WP_ENV_MULTISITE` environment variable override not working correctly

--- packages/env/CHANGELOG.md
+++ packages/env/CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## Unreleased
 
+### Bug Fixes
+
+-   Fix `WP_ENV_MULTISITE` environment variable override not working correctly ([#72567](https://github.com/WordPress/gutenberg/pull/72567)).
+
 ## 10.33.0 (2025-10-17)
 
 ## 10.32.0 (2025-10-01)
--- packages/env/lib/config/parse-config.js
+++ packages/env/lib/config/parse-config.js
@@ -312,6 +312,12 @@ function getEnvironmentVarOverrides( cacheDirectoryPath ) {
 		overrideConfig.env.tests.phpVersion = overrides.phpVersion;
 	}
 
+	if ( overrides.multisite ) {
+		overrideConfig.multisite = overrides.multisite;
+		overrideConfig.env.development.multisite = overrides.multisite;
+		overrideConfig.env.tests.multisite = overrides.multisite;
+	}
+
 	return overrideConfig;
 }
 
