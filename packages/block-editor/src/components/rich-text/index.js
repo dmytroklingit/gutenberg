dbcdc82b38e605d41ce3446d7e50423002c2e7ce
Jerry Jones <jones.jeremydavid@gmail.com>
2025-10-22 10:52:37 -0500

    Revert "Check for RichText bindings from context instead of new subscription"
    This will already be part of a different PR, so removing it here so we can better see the perf impact of early returns from inpsector panels. This reverts commit 703278f9bbb47600075b13c9a13ebbe1c51fba84.

--- packages/block-editor/src/components/rich-text/index.js
+++ packages/block-editor/src/components/rich-text/index.js
@@ -41,7 +41,6 @@ import { getAllowedFormats } from './utils';
 import { Content, valueToHTMLString } from './content';
 import { withDeprecations } from './with-deprecations';
 import BlockContext from '../block-context';
-import { PrivateBlockContext } from '../block-list/private-block-context';
 
 export const keyboardShortcutContext = createContext();
 keyboardShortcutContext.displayName = 'keyboardShortcutContext';
@@ -125,10 +124,9 @@ export function RichTextWrapper(
 	const instanceId = useInstanceId( RichTextWrapper );
 	const anchorRef = useRef();
 	const context = useBlockEditContext();
-	const { clientId, isSelected: isBlockSelected } = context;
+	const { clientId, isSelected: isBlockSelected, name: blockName } = context;
 	const blockBindings = context[ blockBindingsKey ];
 	const blockContext = useContext( BlockContext );
-	const { bindableAttributes } = useContext( PrivateBlockContext );
 	const registry = useRegistry();
 	const selector = ( select ) => {
 		// Avoid subscribing to the block editor store if the block is not
@@ -173,7 +171,15 @@ export function RichTextWrapper(
 
 	const { disableBoundBlock, bindingsPlaceholder, bindingsLabel } = useSelect(
 		( select ) => {
-			if ( ! blockBindings?.[ identifier ] || ! bindableAttributes ) {
+			const { __experimentalBlockBindingsSupportedAttributes } =
+				select( blockEditorStore ).getSettings();
+
+			if (
+				! blockBindings?.[ identifier ] ||
+				! (
+					blockName in __experimentalBlockBindingsSupportedAttributes
+				)
+			) {
 				return {};
 			}
 
@@ -246,7 +252,7 @@ export function RichTextWrapper(
 		[
 			blockBindings,
 			identifier,
-			bindableAttributes,
+			blockName,
 			adjustedValue,
 			clientId,
 			blockContext,
